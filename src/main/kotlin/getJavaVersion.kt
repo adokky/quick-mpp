dd8020910d4c6ba1cca2a7083c448cab38822fb1
Alexander Dokuchaev <doker.al@yandex.com>
2025-06-18 18:24:05 +0300

    get java version from version catalog

--- src/main/kotlin/getJavaVersion.kt
+++ src/main/kotlin/getJavaVersion.kt
@@ -2,9 +2,16 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.getByType
+import kotlin.jvm.optionals.getOrNull
 
-internal fun Project.getJavaVersion(default: Int = 17): Int =
-    properties["kotlin.jvmTarget"].toString().toIntOrNull() ?: default
+internal fun Project.getJavaVersion(default: Int = 17): Int {
+    val versionFromLibs = libs?.findVersion("java")
+        ?.getOrNull()
+        ?.strictVersion
+        ?.takeIf { it.isNotEmpty() }
+    return (versionFromLibs ?: properties["kotlin.jvmTarget"].toString())
+        .toIntOrNull() ?: default
+}
 
-internal val Project.libs: VersionCatalog get() =
-    project.extensions.getByType<VersionCatalogsExtension>().named("libs")
+internal val Project.libs: VersionCatalog? get() =
+    project.extensions.getByType<VersionCatalogsExtension>().find("libs").getOrNull()
